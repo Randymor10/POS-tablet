90b05b60b65b0c313430552c2386c4abf7b4cd62
Randymor10 <randyjrmor10@gmail.com>
2025-06-27 00:03:49 -0700

    Add kiosk menu view with category filtering

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import ReceiptPage from './pages/ReceiptPage'
+import KioskMenuPage from './pages/KioskMenuPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/kiosk" element={<KioskMenuPage />} />
+        <Route path="/receipt" element={<ReceiptPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
